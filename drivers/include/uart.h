63b4d39235c985148050574c81bf89af909d9596
fokaz-c <fokazmchakma4427@gmail.com>
2025-08-28 03:58:42 +0530

    Add copyright and author information to multiple source files

--- drivers/include/uart.h
+++ drivers/include/uart.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Fokaz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef __KUMOTRAIL_UART_H__
 #define __KUMOTRAIL_UART_H__
 
@@ -18,11 +34,11 @@
  *
  * @version 1.0
  * @date 23-08-2025
- * @author ClariS--
- * @maintainer ClariS--
+ * @author Fokaz
+ * @maintainer Fokaz
  *
  * @changelog
- * Version 1.0 - 23-08-2025 - ClariS--
+ * Version 1.0 - 23-08-2025 - Fokaz
  *   - Initial UART driver interface definition
  *   - Basic initialization and transmission functions
  *   - ESP32-C3 hardware integration support
--- include/KumoTrail/sysctl.h
+++ include/KumoTrail/sysctl.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Fokaz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef __KUMOTRAIL_SYSCTL_H__
 #define __KUMOTRAIL_SYSCTL_H__
 
@@ -11,8 +27,8 @@
  *
  * @version 1.0
  * @date 28-08-2025
- * @author ClariS--
- * @maintainer ClariS--
+ * @author Fokaz
+ * @maintainer Fokaz
  *
  * @note This module is essential for the proper functioning of all drivers
  * that interact with hardware peripherals.
